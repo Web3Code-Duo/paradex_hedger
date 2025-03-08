fa1b5cb04008ac78970995c89b85994be727737b
Web3Code-Duo <mailforwork_coder@mail.ru>
2025-03-08 18:25:18 +0500

    Update config.py

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ BITGET_API_KEY = os.environ["BITGET_API_KEY"]
 BITGET_API_SECRET = os.environ["BITGET_API_SECRET"]
 BITGET_API_PASSPHRASE = os.environ["BITGET_API_PASSPHRASE"]
 
-POSITION_USD_LIMITS = [50, 1000]  # min and max position in usd
+POSITION_USD_LIMITS = [101, 1000]  # min and max position in usd
